db9da69a9ecc9636aa7bdbe922f95e2acf42e223
misha <mv.gvozdev@gmail.com>
2018-10-21 13:36:36 +0300

    added Task6

--- src/lesson2/homework/Task6.java
+++ src/lesson2/homework/Task6.java
@@ -3,11 +3,15 @@ package lesson2.homework;
 public class Task6 {
 
     public static void main(String[] args) {
+        int souvenir = 0;
+        for (int ticket = 1; ticket <= 999999; ticket++) {
+            int part1 = ticket / 100000 + ticket / 10000 % 10 + ticket / 1000 % 10;
+            int part2 = ticket / 100 % 10 + ticket / 10 % 10 + ticket % 10;
+            if (part1 == part2) {
+                ++souvenir;
+            }
 
-        for (int ticket = 1; ticket < 999999; ticket++) {
-            if (ticket / 1000000 + ticket / 100000 % 10 + ticket / 10000 % 100 = ticket / 1000 % 1000 + ticket / 100 + ticket /
-
-        }
+        } System.out.println("Количество сувениров: " + souvenir);
 
 
 
