df23d6e00b1d621f3ab9375e6271e59714d21c83
misha <mv.gvozdev@gmail.com>
2018-10-23 22:28:11 +0300

    h/w 3, task 1

--- src/lesson3/homework/Task1.java
+++ src/lesson3/homework/Task1.java
@@ -1,11 +1,28 @@
 package lesson3.homework;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
 
         int count = 0;
-        for (int i=2, i < 21, i + 2);
-        ++count;
+        for (int i = 2; i < 21; i = i + 2) {
+            ++count;
+        }
         System.out.println(count);
+
+        int[] arr = new int[count];
+        int arrIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arrIndex + 2;
+            arrIndex = arrIndex + 2;
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+
+
     }
 }
