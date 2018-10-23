7846547c665bf7593b058033acc84fe388635b77
misha <mv.gvozdev@gmail.com>
2018-10-23 21:57:53 +0300

    task1

---
+++ src/lesson3/homework/Task1.java
@@ -0,0 +1,11 @@
+package lesson3.homework;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i=2, i < 21, i + 2);
+        ++count;
+        System.out.println(count);
+    }
+}
